95d4e53341377b8780cdb6c4c884dacf2dbe2a76
Zeahoo <zeahoooo@gmail.com>
2018-06-04 09:13:53 +0800

    inner bean

--- src/main/java/org/sonny/Application.java
+++ src/main/java/org/sonny/Application.java
@@ -2,16 +2,26 @@ package org.sonny;
 
 import org.sonny.form.ConstructForm;
 import org.sonny.form.HelloForm;
+import org.sonny.form.OutBean;
+import org.sonny.form.PersonForm;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+@SuppressWarnings("resource")
 public class Application {
 
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    // set di
     HelloForm form = context.getBean(HelloForm.class);
     System.out.println(form.getMessage());
+    // construct di
     ConstructForm constructForm = context.getBean(ConstructForm.class);
     System.out.printf("%d -- %s\n", constructForm.getVersion(), constructForm.getName());
+    // inner bean
+    OutBean out = context.getBean(OutBean.class);
+    PersonForm personForm = out.getTarget();
+    System.out.printf("name: %s --- age: %d\n", personForm.getName(), personForm.getAge());
   }
 }
---
+++ src/main/java/org/sonny/form/OutBean.java
@@ -0,0 +1,14 @@
+package org.sonny.form;
+
+public class OutBean {
+  private PersonForm target;
+
+  public PersonForm getTarget() {
+    return target;
+  }
+
+  public void setTarget(PersonForm target) {
+    this.target = target;
+  }
+
+}
